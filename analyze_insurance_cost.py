7f982e0cc17c5531141b3aec5cca5b25d52b26b9
SamtheDeveloper91 <sambudlong7@gmail.com>
2021-08-01 12:30:43 -0400

    Add patients and estimated costs

--- analyze_insurance_cost.py
+++ analyze_insurance_cost.py
@@ -32,3 +32,40 @@ def estimate_insurance_cost(name, age, sex, bmi, num_of_children, smoker):
  
 # Estimate Keanu's insurance cost
 keanu_insurance_cost = estimate_insurance_cost(name = 'Keanu', age = 29, sex = 1, bmi = 37.5, num_of_children = 3, smoker = 1)
+
+# Estimate Maria's insurance cost
+maria_insurance_cost = estimate_insurance_cost(name = "Maria", age = 31, sex = 0, bmi = 23.1, num_of_children = 1, smoker = 0)
+
+# Estimate Rohan's insurance cost
+rohan_insurance_cost = estimate_insurance_cost(name = 
+"Rohan", age = 25, sex = 1, bmi = 28.5, num_of_children = 3, smoker = 0)
+
+# Estimate Valentina's insurance cost
+valentina_insurance_cost = estimate_insurance_cost(name = "Valentina", age = 53, sex = 0, bmi = 31.4, num_of_children = 0, smoker = 1)
+
+# Add your code here
+names = ["Keanu", "Maria", "Rohan", "Valentina"]
+insurance_costs = [33000.0, 4150.0, 5320.0, 35210.0]
+estimated_insurance_costs = [keanu_insurance_cost, maria_insurance_cost, rohan_insurance_cost, valentina_insurance_cost]
+
+insurance_data = list(zip(names, insurance_costs))
+print("\nHere is the actual insurance cost data: " + str(insurance_data) + "\n")
+
+estimated_insurance_data = list(zip(names, estimated_insurance_costs))
+print("Here is the estimated insurance cost data: " + str(estimated_insurance_data)+"\n")
+
+def calculate_insurance_cost_difference(actual_costs, estimated_costs):
+  cost_differences = []
+  for i in range(len(actual_costs)):
+    j, k = estimated_costs[i]
+    l, m = actual_costs[i]
+    if k > m:
+      print(j + "'s estimated insurance cost is " + str(k-m) + " higher than the actual cost.\n")
+    elif k < m:
+      print(j + "'s estimated insurance cost is " + str(m-k) + " lower than the actual cost.")
+    else:
+      print(j + "'s estimated cost is equal to their actual cost!")
+    cost_differences.append(k-m)
+  return cost_differences
+
+calculate_insurance_cost_difference(insurance_data, estimated_insurance_data)
